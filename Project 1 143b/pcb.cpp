1a53307b2d8b5e33cb1b9bdc7a97b87104e2ea5d
AsianSen54tian <ydtian@uci.edu>
2018-02-09 00:14:17 -0800

    PCB refactoring. Removed Constructor
    Removed constructor, unnecessary

--- Project 1 143b/pcb.cpp
+++ Project 1 143b/pcb.cpp
@@ -2,13 +2,5 @@
 #include "status.h"
 #include "rcb.h"
 
-//Constructor
-PCB::PCB(int pID, std::list<RCB> otherResources, std::pair<pStatus, std::list<PCB>*> status)
-{
-	this->pID = pID;
-	this->otherResources = otherResources;
-	this->status = status;
-}
-
 //== overrider
 bool PCB::operator==(const PCB &p) const { return pID == p.pID;}
