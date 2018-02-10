784a9da15e39922eabbef7d6fd82d16025e69d7d
AsianSen54tian <ydtian@uci.edu>
2018-02-10 14:15:17 -0800

    Converted RCB into a struct

--- Project 1 143b/rcb.cpp
+++ Project 1 143b/rcb.cpp
@@ -1,44 +1,47 @@
 #include "rcb.h"
 
 //Constructor
-RCB::RCB(int rID, int resources)
-{
-	this->rID = rID;
-	this->resources = resources;
-}
+//RCB::RCB(int rID, int resources)
+//{
+//	this->rID = rID;
+//	this->resources = resources;
+//}
+//
+////Getters and Setters
+//int RCB::getRID()
+//{
+//	return rID;
+//}
+//
+//int RCB::getResources()
+//{
+//	return resources;
+//}
+//
+//void RCB::incrementResources(int incr)
+//{
+//	this->resources += incr;
+//}
+//
+//void RCB::decreaseResources(int decr)
+//{
+//	this->resources -= decr;
+//}
+//
+//rStatus RCB::getStatus()
+//{
+//	return this->status;
+//}
+//
+//void RCB::setStatus(rStatus status)
+//{
+//	this->status = status;
+//}
+//
+//std::list<PCB> RCB::getWaitingList()
+//{
+//	return waitingList;
+//}
 
-//Getters and Setters
-int RCB::getRID()
-{
-	return rID;
-}
-
-int RCB::getResources()
-{
-	return resources;
-}
-
-void RCB::incrementResources(int incr)
-{
-	this->resources += incr;
-}
-
-void RCB::decreaseResources(int decr)
-{
-	this->resources -= decr;
-}
-
-rStatus RCB::getStatus()
-{
-	return this->status;
-}
-
-void RCB::setStatus(rStatus status)
-{
-	this->status = status;
-}
-
-std::list<PCB> RCB::getWaitingList()
-{
-	return waitingList;
-}
+//Constructor
+bool RCB::operator==(const RCB &r) const { return rID == r.rID; }
--- Project 1 143b/rcb.h
+++ Project 1 143b/rcb.h
@@ -5,22 +5,25 @@
 #include <list>
 #include "pcb.h"
 #include "status.h"
+#include <memory>
 
-class RCB
+struct RCB
 {
-public:
-	RCB(int rID, int resources);
-	int getRID();
-	int getResources();
-	void incrementResources(int incr);
-	void decreaseResources(int decr);
-	rStatus getStatus();
-	void setStatus(rStatus status);
-	std::list<PCB> getWaitingList();
-private:
+//public:
+//	RCB(int rID, int resources);
+//	int getRID();
+//	int getResources();
+//	void incrementResources(int incr);
+//	void decreaseResources(int decr);
+//	rStatus getStatus();
+//	void setStatus(rStatus status);
+//	std::shared_ptr<std::list<PCB>> getWaitingList();
+//private:
+	bool operator==(const RCB&r) const;
+
 	int rID, resources;
 	rStatus status;
-	std::list<PCB> waitingList;
+	std::shared_ptr<std::list<PCB>> waitingList;
 };
 
 #endif //RCB_H
