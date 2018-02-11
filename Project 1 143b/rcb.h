440dae8927187df53f967f4314e342ee503080fb
AsianSen54tian <ydtian@uci.edu>
2018-02-10 17:23:48 -0800

    removed comments from RCB

--- Project 1 143b/rcb.h
+++ Project 1 143b/rcb.h
@@ -8,16 +8,6 @@
 
 struct RCB
 {
-//public:
-//	RCB(int rID, int resources);
-//	int getRID();
-//	int getResources();
-//	void incrementResources(int incr);
-//	void decreaseResources(int decr);
-//	rStatus getStatus();
-//	void setStatus(rStatus status);
-//	std::shared_ptr<std::list<PCB>> getWaitingList();
-//private:
 	bool operator==(const RCB&r) const;
 
 	int rID;
